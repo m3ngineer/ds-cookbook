ed3158bcf50d8d93c9ba35d26addb263f9c23ec8
Matthew Eng <matthew.eng2@gmail.com>
2021-05-01 22:02:26 -0400

    Fix quotation char and indentation issues

--- gan/modern-art-gen/art_gan.py
+++ gan/modern-art-gen/art_gan.py
@@ -33,8 +33,8 @@ def build_discriminator(image_shape):
 
     model = Sequential()
 
-    model.add(Conv2D(32, kernel_size=3, strides=2))
-    input_shape = image_shape, padding="same"))
+    model.add(Conv2D(32, kernel_size=3, strides=2,
+        input_shape = image_shape, padding="same"))
     model.add(LeakyRelU(alpha=0.2))
     model.add(Dropout(0.25))
 
@@ -43,7 +43,7 @@ def build_discriminator(image_shape):
     model.add(LeakyReLU(alpha=0.2))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(128, kernel_size=3, strides=2, padding=”same”))
+    model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
     model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
     model.add(Dropout(0.25))
@@ -69,24 +69,24 @@ def build_discriminator(image_shape):
 # Create generator function
 def build_generator(noise_size, channels):
     model = Sequential()
-    model.add(Dense(4 * 4 * 256, activation=”relu”,       input_dim=noise_size))
+    model.add(Dense(4 * 4 * 256, activation="relu", input_dim=noise_size))
     model.add(Reshape((4, 4, 256)))
     model.add(UpSampling2D())
-    model.add(Conv2D(256, kernel_size=3, padding=”same”))
+    model.add(Conv2D(256, kernel_size=3, padding="same"))
     model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation(“relu”))
+    model.add(Activation("relu"))
     model.add(UpSampling2D())
-    model.add(Conv2D(256, kernel_size=3, padding=”same”))
+    model.add(Conv2D(256, kernel_size=3, padding="same"))
     model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation(“relu”))
+    model.add(Activation("relu"))
     for i in range(GENERATE_RES):
          model.add(UpSampling2D())
-         model.add(Conv2D(256, kernel_size=3, padding=”same”))
+         model.add(Conv2D(256, kernel_size=3, padding="same"))
          model.add(BatchNormalization(momentum=0.8))
-         model.add(Activation(“relu”))
+         model.add(Activation("relu"))
     model.summary()
-    model.add(Conv2D(channels, kernel_size=3, padding=”same”))
-    model.add(Activation(“tanh”))
+    model.add(Conv2D(channels, kernel_size=3, padding="same"))
+    model.add(Activation("tanh"))
     input = Input(shape=(noise_size,))
     generated_image = model(input)
 
@@ -123,35 +123,35 @@ def save_images(cnt, noise):
 image_shape = (IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
 optimizer = Adam(1.5e-4, 0.5)
 discriminator = build_discriminator(image_shape)
-discriminator.compile(loss=”binary_crossentropy”,
-optimizer=optimizer, metrics=[“accuracy”])
+discriminator.compile(loss="binary_crossentropy",
+optimizer=optimizer, metrics=["accuracy"])
 generator = build_generator(NOISE_SIZE, IMAGE_CHANNELS)
 random_input = Input(shape=(NOISE_SIZE,))
 generated_image = generator(random_input)
 discriminator.trainable = False
 validity = discriminator(generated_image)
 combined = Model(random_input, validity)
-combined.compile(loss=”binary_crossentropy”,
-optimizer=optimizer, metrics=[“accuracy”])
+combined.compile(loss="binary_crossentropy",
+optimizer=optimizer, metrics=["accuracy"])
 y_real = np.ones((BATCH_SIZE, 1))
 y_fake = np.zeros((BATCH_SIZE, 1))
 fixed_noise = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, NOISE_SIZE))
 cnt = 1
 for epoch in range(EPOCHS):
- idx = np.random.randint(0, training_data.shape[0], BATCH_SIZE)
- x_real = training_data[idx]
+    idx = np.random.randint(0, training_data.shape[0], BATCH_SIZE)
+    x_real = training_data[idx]
 
- noise= np.random.normal(0, 1, (BATCH_SIZE, NOISE_SIZE))
- x_fake = generator.predict(noise)
+    noise= np.random.normal(0, 1, (BATCH_SIZE, NOISE_SIZE))
+    x_fake = generator.predict(noise)
 
- discriminator_metric_real = discriminator.train_on_batch(x_real, y_real)
-discriminator_metric_generated = discriminator.train_on_batch(
- x_fake, y_fake)
+    discriminator_metric_real = discriminator.train_on_batch(x_real, y_real)
+    discriminator_metric_generated = discriminator.train_on_batch(
+    x_fake, y_fake)
 
-discriminator_metric = 0.5 * np.add(discriminator_metric_real, discriminator_metric_generated)
-generator_metric = combined.train_on_batch(noise, y_real)
-if epoch % SAVE_FREQ == 0:
-   save_images(cnt, fixed_noise)
-   cnt += 1
+    discriminator_metric = 0.5 * np.add(discriminator_metric_real, discriminator_metric_generated)
+    generator_metric = combined.train_on_batch(noise, y_real)
+    if epoch % SAVE_FREQ == 0:
+        save_images(cnt, fixed_noise)
+        cnt += 1
 
-   print(f”{epoch} epoch, Discriminator accuracy: {100*  discriminator_metric[1]}, Generator accuracy: {100 * generator_metric[1]}”)
+    print(f"{epoch} epoch, Discriminator accuracy: {100*  discriminator_metric[1]}, Generator accuracy: {100 * generator_metric[1]}")
