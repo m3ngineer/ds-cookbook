517a07ac9b4930f8bfe5f7ecbe4428ac1dd0d723
Matthew Eng <matthew.eng2@gmail.com>
2021-05-23 15:51:09 -0400

    Add settings.py

---
+++ gan/modern-art-gen/settings.py
@@ -0,0 +1,6 @@
+import os
+
+# Path to training dataset
+ORIGINAL_IMAGES_PATH = os.environ.get('ORIGINAL_IMAGES_PATH')
+# Path to store thumbnails of training dataset
+RESIZED_IMAGES_PATH = os.environ.get('RESIZED_IMAGES_PATH')
