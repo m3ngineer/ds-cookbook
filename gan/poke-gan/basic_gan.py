5a70c46a7fc98f50e28ca4e0c39f2e7524b3103d
Matthew Eng <matthew.eng2@gmail.com>
2021-06-12 21:57:59 -0400

    Replace with boilerplate pytorch code

--- gan/poke-gan/basic_gan.py
+++ gan/poke-gan/basic_gan.py
@@ -1,87 +1,186 @@
-import tensorflow as tf
+#!/usr/bin/env python
+
+# Generative Adversarial Networks (GAN) example in PyTorch. Tested with PyTorch 0.4.1, Python 3.6.7 (Nov 2018)
+# See related blog post at https://medium.com/@devnag/generative-adversarial-networks-gans-in-50-lines-of-code-pytorch-e81b79659e3f#.sch4xgsa9
+
 import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow import keras
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
 
-from discriminator_loss import discriminator_objective
-from discriminator_net import Discriminator
-from generator_objective import generator_objective
-from training_gan import training
-# from training_step import training_step
+matplotlib_is_available = True
+try:
+  from matplotlib import pyplot as plt
+except ImportError:
+  print("Will skip plotting; matplotlib is not available.")
+  matplotlib_is_available = False
 
-print(tf.__version__)
+# Data params
+data_mean = 4
+data_stddev = 1.25
+
+# ### Uncomment only one of these to define what data is actually sent to the Discriminator
+#(name, preprocess, d_input_func) = ("Raw data", lambda data: data, lambda x: x)
+#(name, preprocess, d_input_func) = ("Data and variances", lambda data: decorate_with_diffs(data, 2.0), lambda x: x * 2)
+#(name, preprocess, d_input_func) = ("Data and diffs", lambda data: decorate_with_diffs(data, 1.0), lambda x: x * 2)
+(name, preprocess, d_input_func) = ("Only 4 moments", lambda data: get_moments(data), lambda x: 4)
+
+print("Using data [%s]" % (name))
+
+# ##### DATA: Target data and generator input data
+
+def get_distribution_sampler(mu, sigma):
+    return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))  # Gaussian
+
+def get_generator_input_sampler():
+    return lambda m, n: torch.rand(m, n)  # Uniform-dist data into generator, _NOT_ Gaussian
+
+# ##### MODELS: Generator model and discriminator model
+
+class Generator(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, f):
+        super(Generator, self).__init__()
+        self.map1 = nn.Linear(input_size, hidden_size)
+        self.map2 = nn.Linear(hidden_size, hidden_size)
+        self.map3 = nn.Linear(hidden_size, output_size)
+        self.f = f
+
+    def forward(self, x):
+        x = self.map1(x)
+        x = self.f(x)
+        x = self.map2(x)
+        x = self.f(x)
+        x = self.map3(x)
+        return x
+
+class Discriminator(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, f):
+        super(Discriminator, self).__init__()
+        self.map1 = nn.Linear(input_size, hidden_size)
+        self.map2 = nn.Linear(hidden_size, hidden_size)
+        self.map3 = nn.Linear(hidden_size, output_size)
+        self.f = f
+
+    def forward(self, x):
+        x = self.f(self.map1(x))
+        x = self.f(self.map2(x))
+        return self.f(self.map3(x))
+
+def extract(v):
+    return v.data.storage().tolist()
+
+def stats(d):
+    return [np.mean(d), np.std(d)]
+
+def get_moments(d):
+    # Return the first 4 moments of the data provided
+    mean = torch.mean(d)
+    diffs = d - mean
+    var = torch.mean(torch.pow(diffs, 2.0))
+    std = torch.pow(var, 0.5)
+    zscores = diffs / std
+    skews = torch.mean(torch.pow(zscores, 3.0))
+    kurtoses = torch.mean(torch.pow(zscores, 4.0)) - 3.0  # excess kurtosis, should be 0 for Gaussian
+    final = torch.cat((mean.reshape(1,), std.reshape(1,), skews.reshape(1,), kurtoses.reshape(1,)))
+    return final
+
+def decorate_with_diffs(data, exponent, remove_raw_data=False):
+    mean = torch.mean(data.data, 1, keepdim=True)
+    mean_broadcast = torch.mul(torch.ones(data.size()), mean.tolist()[0][0])
+    diffs = torch.pow(data - Variable(mean_broadcast), exponent)
+    if remove_raw_data:
+        return torch.cat([diffs], 1)
+    else:
+        return torch.cat([data, diffs], 1)
+
+def train():
+    # Model parameters
+    g_input_size = 1      # Random noise dimension coming into generator, per output vector
+    g_hidden_size = 5     # Generator complexity
+    g_output_size = 1     # Size of generated output vector
+    d_input_size = 500    # Minibatch size - cardinality of distributions
+    d_hidden_size = 10    # Discriminator complexity
+    d_output_size = 1     # Single dimension for 'real' vs. 'fake' classification
+    minibatch_size = d_input_size
+
+    d_learning_rate = 1e-3
+    g_learning_rate = 1e-3
+    sgd_momentum = 0.9
+
+    num_epochs = 5000
+    print_interval = 100
+    d_steps = 20
+    g_steps = 20
+
+    dfe, dre, ge = 0, 0, 0
+    d_real_data, d_fake_data, g_fake_data = None, None, None
+
+    discriminator_activation_function = torch.sigmoid
+    generator_activation_function = torch.tanh
+
+    d_sampler = get_distribution_sampler(data_mean, data_stddev)
+    gi_sampler = get_generator_input_sampler()
+    G = Generator(input_size=g_input_size,
+                  hidden_size=g_hidden_size,
+                  output_size=g_output_size,
+                  f=generator_activation_function)
+    D = Discriminator(input_size=d_input_func(d_input_size),
+                      hidden_size=d_hidden_size,
+                      output_size=d_output_size,
+                      f=discriminator_activation_function)
+    criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
+    d_optimizer = optim.SGD(D.parameters(), lr=d_learning_rate, momentum=sgd_momentum)
+    g_optimizer = optim.SGD(G.parameters(), lr=g_learning_rate, momentum=sgd_momentum)
+
+    for epoch in range(num_epochs):
+        for d_index in range(d_steps):
+            # 1. Train D on real+fake
+            D.zero_grad()
+
+            #  1A: Train D on real
+            d_real_data = Variable(d_sampler(d_input_size))
+            d_real_decision = D(preprocess(d_real_data))
+            d_real_error = criterion(d_real_decision, Variable(torch.ones([1,1])))  # ones = true
+            d_real_error.backward() # compute/store gradients, but don't change params
+
+            #  1B: Train D on fake
+            d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+            d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
+            d_fake_decision = D(preprocess(d_fake_data.t()))
+            d_fake_error = criterion(d_fake_decision, Variable(torch.zeros([1,1])))  # zeros = fake
+            d_fake_error.backward()
+            d_optimizer.step()     # Only optimizes D's parameters; changes based on stored gradients from backward()
+
+            dre, dfe = extract(d_real_error)[0], extract(d_fake_error)[0]
+
+        for g_index in range(g_steps):
+            # 2. Train G on D's response (but DO NOT train D on these labels)
+            G.zero_grad()
+
+            gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+            g_fake_data = G(gen_input)
+            dg_fake_decision = D(preprocess(g_fake_data.t()))
+            g_error = criterion(dg_fake_decision, Variable(torch.ones([1,1])))  # Train G to pretend it's genuine
+
+            g_error.backward()
+            g_optimizer.step()  # Only optimizes G's parameters
+            ge = extract(g_error)[0]
+
+        if epoch % print_interval == 0:
+            print("Epoch %s: D (%s real_err, %s fake_err) G (%s err); Real Dist (%s),  Fake Dist (%s) " %
+                  (epoch, dre, dfe, ge, stats(extract(d_real_data)), stats(extract(d_fake_data))))
+
+    if matplotlib_is_available:
+        print("Plotting the generated distribution...")
+        values = extract(g_fake_data)
+        print(" Values: %s" % (str(values)))
+        plt.hist(values, bins=50)
+        plt.xlabel('Value')
+        plt.ylabel('Count')
+        plt.title('Histogram of Generated Distribution')
+        plt.grid(True)
+        plt.show()
 
 
-BATCH_SIZE = 256
-BUFFER_SIZE = 60000
-EPOCHES = 300
-OUTPUT_DIR = "img" # The output directory where the images of the generator a stored during training
-
-
-mnist = keras.datasets.mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-print(train_images.shape)
-
-plt.imshow(train_images[1], cmap = "gray")
-
-train_images = train_images.astype("float32")
-train_images = (train_images - 127.5) / 127.5
-
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images.reshape(train_images.shape[0],784)).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
-
-class Generator(keras.Model):
-
-    def __init__(self, random_noise_size = 100):
-        super().__init__(name='generator')
-        #layers
-        self.input_layer = keras.layers.Dense(units = random_noise_size)
-        self.dense_1 = keras.layers.Dense(units = 128)
-        self.leaky_1 =  keras.layers.LeakyReLU(alpha = 0.01)
-        self.dense_2 = keras.layers.Dense(units = 128)
-        self.leaky_2 = keras.layers.LeakyReLU(alpha = 0.01)
-        self.dense_3 = keras.layers.Dense(units = 256)
-        self.leaky_3 = keras.layers.LeakyReLU(alpha = 0.01)
-        self.output_layer = keras.layers.Dense(units=784, activation = "tanh")
-
-    def call(self, input_tensor):
-        ## Definition of Forward Pass
-        x = self.input_layer(input_tensor)
-        x = self.dense_1(x)
-        x = self.leaky_1(x)
-        x = self.dense_2(x)
-        x = self.leaky_2(x)
-        x = self.dense_3(x)
-        x = self.leaky_3(x)
-        return  self.output_layer(x)
-
-    def generate_noise(self,batch_size, random_noise_size):
-        return np.random.uniform(-1,1, size = (batch_size, random_noise_size))
-
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits = True)
-
-@tf.function()
-def training_step(generator: Discriminator, discriminator: Discriminator, images:np.ndarray , k:int =1, batch_size = 32):
-    for _ in range(k):
-         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-            noise = generator.generate_noise(batch_size, 100)
-            g_z = generator(noise)
-            d_x_true = discriminator(images) # Trainable?
-            d_x_fake = discriminator(g_z) # dx_of_gx
-
-            discriminator_loss = discriminator_objective(d_x_true, d_x_fake)
-            # Adjusting Gradient of Discriminator
-            gradients_of_discriminator = disc_tape.gradient(discriminator_loss, discriminator.trainable_variables)
-            discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables)) # Takes a list of gradient and variables pairs
-
-
-            generator_loss = generator_objective(d_x_fake)
-            # Adjusting Gradient of Generator
-            gradients_of_generator = gen_tape.gradient(generator_loss, generator.trainable_variables)
-            generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-
-generator_optimizer = keras.optimizers.RMSprop()
-discriminator_optimizer = keras.optimizers.RMSprop()
-
-
-%%time
-training(train_dataset, EPOCHES)
+train()
