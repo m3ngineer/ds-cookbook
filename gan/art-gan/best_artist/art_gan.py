f5f5940874c2a88d6aeb1e596f0c4b1c99f2996b
Matthew Eng <matthew.eng2@gmail.com>
2021-11-27 20:59:11 -0500

    Use Jovian code

---
+++ gan/art-gan/best_artist/art_gan.py
@@ -0,0 +1,288 @@
+# I always like to structure my imports into Python's native libs,
+# stuff I installed via conda/pip and local file imports (we don't have those here)
+import os
+import re
+import time
+import enum
+
+
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import git
+
+
+import torch
+from torch import nn
+from torch.optim import Adam
+from torchvision import transforms, datasets
+from torchvision.utils import make_grid, save_image
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+# Let's create some constant to make stuff a bit easier
+BINARIES_PATH = os.path.join(os.getcwd(), 'models', 'binaries')  # location where trained models are located
+CHECKPOINTS_PATH = os.path.join(os.getcwd(), 'models', 'checkpoints')  # semi-trained models during training will be dumped here
+DATA_DIR_PATH = os.path.join(os.getcwd(), 'data')  # all data both input (MNIST) and generated will be stored here
+DEBUG_IMAGERY_PATH = os.path.join(DATA_DIR_PATH, 'debug_imagery')  # we'll be dumping images here during GAN training
+
+os.makedirs(BINARIES_PATH, exist_ok=True)
+os.makedirs(CHECKPOINTS_PATH, exist_ok=True)
+os.makedirs(DEBUG_IMAGERY_PATH, exist_ok=True)
+
+MNIST_IMG_SIZE = 64  # MNIST images have 28x28 resolution, it's just convinient to put this into a constant you'll see later why
+
+batch_size = 128
+image_size = (64,64)
+stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+
+# Images are usually in the [0., 1.] or [0, 255] range, Normalize transform will bring them into [-1, 1] range
+# It's one of those things somebody figured out experimentally that it works (without special theoretical arguments)
+# https://github.com/soumith/ganhacks <- you can find more of those hacks here
+transform = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(*stats)
+    ])
+
+train_dataset = datasets.ImageFolder(root=os.path.join(DATA_DIR_PATH,'resized'),
+                                     transform=transform)
+
+train_data_loader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=3, pin_memory=True)
+
+images,_ = train_dataset[382]
+print(images.size())
+plt.imshow(images.permute(1,2,0))
+# plt.show()
+# exit()
+# Size of the generator's input vector. Generator will eventually learn how to map these into meaningful images!
+# LATENT_SPACE_DIM = 100
+#
+# latent_size = 150
+#
+# # This one will produce a batch of those vectors
+# def get_gaussian_latent_batch(batch_size, device):
+#     return torch.randn((batch_size, LATENT_SPACE_DIM), device=device)
+#
+#
+# # It's cleaner if you define the block like this - bear with me
+# def vanilla_block(in_feat, out_feat, normalize=True, activation=None):
+#     layers = [nn.Linear(in_feat, out_feat)]
+#     if normalize:
+#         layers.append(nn.BatchNorm1d(out_feat))
+#     # 0.2 was used in DCGAN, I experimented with other values like 0.5 didn't notice significant change
+#     layers.append(nn.LeakyReLU(0.2) if activation is None else activation)
+#     return layers
+
+def denorm(img_tensors):
+    return img_tensors * stats[1][0] + stats[0][0]
+
+def show_images(images, nmax=64):
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.set_xticks([]); ax.set_yticks([])
+    ax.imshow(make_grid(denorm(images.detach()[:nmax]), nrow=8).permute(1, 2, 0))
+
+def show_batch(dl, nmax=64):
+    for images, _ in dl:
+        show_images(images, nmax)
+        break
+        
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+device = get_default_device()
+
+train_data_loader = DeviceDataLoader(train_data_loader, device)
+
+discriminator = nn.Sequential(
+    # in: 3 x 64 x 64
+
+    nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 64 x 32 x 32
+
+    nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 128 x 16 x 16
+
+    nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 256 x 8 x 8
+
+    nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(512),
+    nn.LeakyReLU(0.2, inplace=True),
+    # out: 512 x 4 x 4
+
+    nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+    # out: 1 x 1 x 1
+
+    nn.Flatten(),
+    nn.Sigmoid())
+discriminator = to_device(discriminator, device)
+
+latent_size = 150
+
+generator = nn.Sequential(
+    # in: latent_size x 1 x 1
+
+    nn.ConvTranspose2d(latent_size, 512, kernel_size=4, stride=1, padding=0, bias=False),
+    nn.BatchNorm2d(512),
+    nn.ReLU(True),
+    # out: 512 x 4 x 4
+
+    nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.ReLU(True),
+    # out: 256 x 8 x 8
+
+    nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.ReLU(True),
+    # out: 128 x 16 x 16
+
+    nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.ReLU(True),
+    # out: 64 x 32 x 32
+
+    nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.Tanh()
+    # out: 3 x 64 x 64
+)
+
+xb = torch.randn(batch_size, latent_size, 1, 1) # random latent tensors
+fake_images = generator(xb)
+generator = to_device(generator, device)
+
+def train_discriminator(real_images, opt_d):
+    # Clear discriminator gradients
+    opt_d.zero_grad()
+
+    # Pass real images through discriminator
+    real_preds = discriminator(real_images)
+    real_targets = torch.ones(real_images.size(0), 1, device=device)
+    real_loss = F.binary_cross_entropy(real_preds, real_targets)
+    real_score = torch.mean(real_preds).item()
+
+    # Generate fake images
+    latent = torch.randn(batch_size, latent_size, 1, 1, device=device)
+    fake_images = generator(latent)
+
+    # Pass fake images through discriminator
+    fake_targets = torch.zeros(fake_images.size(0), 1, device=device)
+    fake_preds = discriminator(fake_images)
+    fake_loss = F.binary_cross_entropy(fake_preds, fake_targets)
+    fake_score = torch.mean(fake_preds).item()
+
+    # Update discriminator weights
+    loss = real_loss + fake_loss
+    loss.backward()
+    opt_d.step()
+    return loss.item(), real_score, fake_score
+
+def train_generator(opt_g):
+    # Clear generator gradients
+    opt_g.zero_grad()
+
+    # Generate fake images
+    latent = torch.randn(batch_size, latent_size, 1, 1, device=device)
+    fake_images = generator(latent)
+
+    # Try to fool the discriminator
+    preds = discriminator(fake_images)
+    targets = torch.ones(batch_size, 1, device=device)
+    loss = F.binary_cross_entropy(preds, targets)
+
+    # Update generator weights
+    loss.backward()
+    opt_g.step()
+
+    return loss.item()
+
+
+sample_dir = 'generated'
+os.makedirs(sample_dir, exist_ok=True)
+
+def save_samples(index, latent_tensors, show=True):
+    fake_images = generator(latent_tensors)
+    fake_fname = 'generated-images-{0:0=4d}.png'.format(index)
+    save_image(denorm(fake_images), os.path.join(sample_dir, fake_fname), nrow=8)
+    print('Saving', fake_fname)
+    if show:
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(fake_images.cpu().detach(), nrow=8).permute(1, 2, 0))
+
+fixed_latent = torch.randn(64, latent_size, 1, 1, device=device)
+
+save_samples(0, fixed_latent)
+
+import torch.nn.functional as F
+
+def fit(epochs, lr, start_idx=1):
+    torch.cuda.empty_cache()
+
+    # Losses & scores
+    losses_g = []
+    losses_d = []
+    real_scores = []
+    fake_scores = []
+
+    # Create optimizers
+    opt_d = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
+    opt_g = torch.optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
+
+    for epoch in range(epochs):
+        for batch_idx, (real_images, _) in enumerate(train_data_loader):
+            # Train discriminator
+            loss_d, real_score, fake_score = train_discriminator(real_images, opt_d)
+            # Train generator
+            loss_g = train_generator(opt_g)
+
+        # Record losses & scores
+        losses_g.append(loss_g)
+        losses_d.append(loss_d)
+        real_scores.append(real_score)
+        fake_scores.append(fake_score)
+
+        # Log losses & scores (last batch)
+        print("Epoch [{}/{}], loss_g: {:.4f}, loss_d: {:.4f}, real_score: {:.4f}, fake_score: {:.4f}".format(
+            epoch+1, epochs, loss_g, loss_d, real_score, fake_score))
+
+        # Save generated images
+        save_samples(epoch+start_idx, fixed_latent, show=False)
+
+    return losses_g, losses_d, real_scores, fake_scores
+
+lr = 0.001
+epochs = 150
+history = fit(epochs,lr)
